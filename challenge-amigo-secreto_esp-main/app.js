b7fc1cc5e134bcb492c4eed0848831b5d385c85e
marlon miranda <mym2393@gmail.com>
2025-01-30 13:04:25 -0500

    creacion de lista de amigos vacia y primera funcion (agregar amigo)

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+// desarrollamos una lista de amigos
+
+let listaAmigos = [];
+
+
+function borrarCampo(){
+    document.getElementById("amigo").value = "";
+}
+
+function agregarAmigo(){
+    if(document.getElementById("amigo").value !== ""){
+    listaAmigos.push(document.getElementById("amigo").value);
+    borrarCampo();
+    } else {
+    alert("Por favor, ingrese un nombre válido.");
+    }
+}
+
